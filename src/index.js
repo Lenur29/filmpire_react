9d0dea0fe536c7e00d4574705141f7c677e03ffe
Lenur Azizov <fd.lenurazizov@gmail.com>
2024-12-16 22:08:13 +0200

    Replaced BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToggleColorModeProvider } from './utils/ToggleColorMode.jsx';
 import App from './components/App.jsx';
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <ToggleColorModeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ToggleColorModeProvider>
   </Provider>,
 );
